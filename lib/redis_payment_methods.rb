0e4de1156e23db90e9415f3c975b864fc15a889f
Slaurmagan <hrakovich.dev@gmail.com>
2024-12-17 13:59:25 +0400

    Get payment methods from payment

--- lib/redis_payment_methods.rb
+++ lib/redis_payment_methods.rb
@@ -5,5 +5,9 @@ class RedisPaymentMethods
     def fetch
       JSON.parse($redis.get('payment_page:payment_methods').presence || '[]')
     end
+
+    def from_payment(payment)
+      RedisPayment.fetch(payment['id'])['payment_methods'] || []
+    end
   end
 end
