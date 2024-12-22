97144af10d4d2ec5a0e059fe1d3634120ebe084a
Slaurmagan <hrakovich.dev@gmail.com>
2024-12-22 18:15:16 +0400

    Remove mount cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  mount ActionCable.server => '/cable'
-
   resources :payments, only: :show do
     member do
       post :assign_payment_method
