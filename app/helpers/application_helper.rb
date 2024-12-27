d681978785a26862350fe71df88be29729ca50b6
Slaurmagan <hrakovich.dev@gmail.com>
2024-12-28 00:20:59 +0400

    Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def fingerprint_tag(api_key)
-    return content_tag :div if apiKey.blank?
+    return content_tag :div if api_key.blank?
 
     content_tag :div, '', data: { controller: 'fingerprint', fingerprint_api_key_value: api_key }
   end
