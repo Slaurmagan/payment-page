f726bbd2578cca929c88b3d34fa62282e6d4b2e6
Slaurmagan <hrakovich.dev@gmail.com>
2025-01-06 22:23:01 +0400

    Decompress payment before evaluate to json

--- lib/redis_payment.rb
+++ lib/redis_payment.rb
@@ -3,8 +3,9 @@ class RedisPayment
 
   class << self
     def fetch(id)
-      val = $redis.get("payment_page:payments:#{id}").presence || '{}'
-      JSON.parse(val).with_indifferent_access rescue {}
+      val = $redis.get("payment_page:payments:#{id}").presence || "x\x9C\xAB\xAE\x05\x00\x01u\x00\xF9"
+
+      JSON.parse(Zlib.inflate(val)).with_indifferent_access rescue {}
     end
   end
 end
