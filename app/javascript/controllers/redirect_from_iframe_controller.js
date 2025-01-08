4ddece4871e013a8a3c2bfc30e80a7af270d6539
Slaurmagan <hrakovich.dev@gmail.com>
2025-01-09 03:47:30 +0400

    Remove console logs

--- app/javascript/controllers/redirect_from_iframe_controller.js
+++ app/javascript/controllers/redirect_from_iframe_controller.js
@@ -2,9 +2,7 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log('checking if in iframe')
     if (window.self !== window.top) {
-      console.log('iframe opened')
       window.open(window.location.href, '_blank');
 
       window.top.location = 'about:blank'; // Or redirect it elsewhere
